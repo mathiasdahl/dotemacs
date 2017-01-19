1ac08088d21a12737471cb603709fadec9a0001e
mathiasdahl <mathias.dahl@gmail.com>
2017-01-19 15:33:25 +0100

    Add support for MELPA

--- barebones.el
+++ barebones.el
@@ -37,3 +37,10 @@
                          (kill-buffer (current-buffer)))))
 (global-set-key [f9] 'toggle-truncate-lines)
 
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
